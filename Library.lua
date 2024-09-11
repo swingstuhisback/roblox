5d6429c6139ee00ceb01e6693153dba9174ac080
swingstuh <167443442+swingstuhisback@users.noreply.github.com>
2024-09-11 22:05:54 +0200

    Create Library.lua

---
+++ Library.lua
@@ -0,0 +1,30 @@
+getgenv().servicesTest = true -- Runs a fast test to see if all services work.
+
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local Services = {
+    CollectionService = game:GetService("CollectionService"),
+    ContextActionService = game:GetService("ContextActionService"),
+    CoreGui = game:GetService("CoreGui"),
+    DataStoreService = game:GetService("DataStoreService"),
+    Lighting = game:GetService("Lighting"),
+    LocalPlayer = game:GetService("Players").LocalPlayer,
+    MarketplaceService = game:GetService("MarketplaceService"),
+    MemoryStoreService = game:GetService("MemoryStoreService"),
+    MessagingService = game:GetService("MessagingService"),
+    Players = game:GetService("Players"),
+    ReplicatedFirst = game:GetService("ReplicatedFirst"),
+    ReplicatedStorage = game:GetService("ReplicatedStorage"),
+    RunService = game:GetService("RunService"),
+    SoundService = game:GetService("SoundService"),
+    TweenService = game:GetService("TweenService"),
+    Workspace = game:GetService("Workspace")
+}
+
+local Utils = {}
+
+Utils.lineBreak = function(amt) amt = amt or 1 for i = 1, amt do print("\n") end end
+
+if servicesTest == true then print"running services test" Utils.lineBreak() for i, v in pairs(Services) do print(i .. " = " .. tostring(v)) end Utils.lineBreak() end
